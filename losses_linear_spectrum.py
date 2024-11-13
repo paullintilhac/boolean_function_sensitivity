2b469fe913b14aeade89d3da7cb1f494bfca888a
paul-s-lintilhac-th_usma <paul.s.lintilhac.th@guest.westpoint.edu>
2024-11-13 16:45:26 -0500

    after pasting from wire

--- losses_linear_spectrum.py
+++ losses_linear_spectrum.py
@@ -2,178 +2,78 @@
 # Functions whose Fourier degree is concentrated on higher weights are harder to learn for LSTMs with SGD
 
 import numpy as np
+import pandas as pd
 import torch
+from torch.utils.data import DataLoader
 import random
-from math import log
-import math
-import sys
-import os
 import argparse
-__file__ = "linear_spectrum_small"
+from transformer import Transformer
+import os
+import itertools
+from torch.nn.parallel import DistributedDataParallel as DDP
 
-print("done loading")
-mps_avail = torch.backends.mps.is_available()
-cuda_avail = torch.cuda.is_available()
-N = 30 #random.randint(2,30)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+if torch.cuda.is_available():
+    print("GPU is available")
+    print("device count: " + str(torch.cuda.device_count()))
+else:
+    print("GPU is not available")
 
+def fitNetwork(function, loader, N, epochs, dir_name,n_devices):
+    lr = 3e-5
+    weight_decay = .0001
+    model = torch.nn.DataParallel(Transformer(N, args.dim, args.h, args.l, args.f, 1e-8).to(device),device_ids=range(n_devices))
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
+    model.train()
+    outer_break = False
+    movAvg = 0
+    summary = pd.DataFrame(columns=["iter", "loss"])
+    # dir_name = f"{args.N}_{args.dim}_{args.l}_{args.h}_{args.f}"
+    
+    for epoch in range(epochs):   
+        epoch_loss = 0
+        total_records = 0
+        #print("loader[0]: " + str(loader[0]))
+        for idx, inputs in enumerate(loader):
+          #print("inputs shape: "  + str(inputs.shape))
+          #print("idx: " + str(idx))
+          
+          model.train()
+          targets = torch.FloatTensor([float(function(x)) for x in inputs]).to(device)
+          result = model(inputs)
+          #loss = -(result*targets).mean()
+          loss =  (result-targets).pow(2).mean()
+          epoch_loss+=loss.detach().cpu()*len(inputs)
+          total_records+=len(inputs)
 
-if mps_avail:
-  device = torch.device("mps")
-elif cuda_avail:
-   device = torch.device("cuda")
-print("cuda is available?" + str(cuda_avail) + ", mps available? " + str(mps_avail))
-TEMPERATURE = 1
+          (loss).backward()
+          optimizer.step()
+          optimizer.zero_grad()
+        
+          iteration = epoch*len(loader)+idx+1
+          # epoch_loss = epoch_loss.detach().cpu()
+                    
+        epoch_loss/=float(total_records)
+        if epoch_loss < 0.01:
+          break	
+        if (epoch) % 5 == 0:
+            #print("iter: " + str(iteration) + ", loss: " +str(movAvg))
+            print("iterations: " + str(iteration) + ", total records: " + str(total_records) + ", epoch: " + str(epoch) + ", epoch_loss: " + str(epoch_loss))
 
-print ('argument list', sys.argv)
-print("after argument list")
+            summary.loc[len(summary)] = {"epoch":epoch,"iterations":iteration, "epoch_loss":epoch_loss}
+            summary.to_csv(f"{dir_name}/curr_func.csv")
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='linear spectrum non boolean test.')
-    parser.add_argument('--d', type=int, default=8)
-    parser.add_argument('--f', type=int, default=8)
-    parser.add_argument('--l', type=int, default=2)
-    parser.add_argument('--h', type=int, default=2)
-    parser.add_argument('--i', type=int, default=300000)
-    return parser.parse_args()
+        if (epoch) % 20 == 0:
+            val_loss = validate(model, function, num_samples=1000)
+            print(f"Iterations: {iteration}, Epoch: {epoch}, EpochLoss: {epoch_loss:.3f}, ValidationLoss: {val_loss:.3f}, TotalRecords: {total_records:.3f}")
+            path = os.path.join(dir_name, f"model_{epoch}.pt")
+            torch.save(model.state_dict(), path)  
 
-print("before args partse")
-args = parse_args()
-print("after args parse")
-print("args: " + str(args))
-folder_string = "d_"+str(args.d)+"-f_"+str(args.f)+"-l_"+str(args.l) + "-h_"+str(args.h)
-#if not os.path.exists(folder_string):
-#    os.mkdir(folder_string)
-print("folder_string: " + folder_string)
-#print(weights)
+    return model, summary
 
-# https://wiki.python.org/moin/BitManipulation
-def parityOf(int_type):
-        # now print the int in binary
-#        print(int_type, "{0:b}".format(int_type))
-# #      parity__ = (degree(int_type) % 2)
- #       parity_ = len([y for y in "{0:b}".format(int_type) if y == "1"]) % 2
-        parity = 0
-        while (int_type):
-            parity = ~parity
-            int_type = int_type & (int_type - 1)
-#        print(parity, parity_, parity__)
-        assert parity in [-1, 0]
-        parity = 2*parity + 1
-        return(parity)
-
-
-def degree(subset):
-   return len([y for y in "{0:b}".format(subset) if y == "1"])
-
-
-import math
-# randomize the function using the orthonormal basis
-
-import torch
-import random
-
-hidden_size = args.d
-batch_size = 2
-
-def makeBitTensor(x, N):
-  y = format(x, "b")
-  y = ("0"*(N-len(y))) + y
-  return [int(z) for z in list(y)]
-
-def validate(model,output, func, num_samples=1000):
-      model.eval()
-      embeddings = torch.nn.Embedding(2, hidden_size//2).to(device)
-      positional_embeddings = torch.nn.Embedding(N, hidden_size//2).to(device)
-
-      inputs = [random.randint(0, 2**N-1) for _ in range(num_samples)]
-      targets = torch.FloatTensor([float(func(x)) for x in inputs]).to(device)
-  #   print(targets.mean())
-  #   quit()
-      inputNum = torch.LongTensor([makeBitTensor(x,N) for x in inputs]).to(device).t()
-      positional = torch.LongTensor(list(range(0, N))).unsqueeze(1).expand(-1, num_samples).to(device)
-
-      num_embeddings = embeddings(inputNum)
-      pos_embed = positional_embeddings(positional)
-      #print("shape num embeddings: " + str(num_embeddings.shape) + ", pos_embed: " + str(pos_embed.shape))
-      inputTensorEmbed = torch.cat([num_embeddings, pos_embed], dim=2)  
-      hidden = model(inputTensorEmbed)[0]
-      result = (output(hidden)).view(-1)
-      # print(result.size())
-      result = result.view(num_samples)
-      loss = (result - targets).pow(2).mean()
-      return loss.detach()
-
-def fitNetwork(function, N):
-   embeddings = torch.nn.Embedding(2, hidden_size//2).to(device)
-   positional_embeddings = torch.nn.Embedding(N, hidden_size//2).to(device)
-   qrnn = torch.nn.TransformerEncoder(encoder_layer = torch.nn.TransformerEncoderLayer(d_model=hidden_size, nhead=args.h, dim_feedforward=args.f, dropout=0.0, activation='relu'), num_layers=args.l).to(device)
-   
-   output = torch.nn.Linear(hidden_size, 1, bias=False).to(device)
-  # Flatten and dot
-   
-   tanh = torch.nn.Tanh()
-   
-   def parameters():
-     for x in [embeddings, qrnn, output, positional_embeddings]:
-       for y in x.parameters():
-          yield y
-   
-   optimizer = torch.optim.AdamW(parameters(), lr=0.00003, weight_decay=0.1)
-
-   movAvg = 0
-   lossesAfterIterations = [] 
-   for iteration in range(args.i):
-     optimizer.zero_grad()
-     inputs = [random.randint(0, 2**N-1) for _ in range(batch_size)]
-     targets = torch.FloatTensor([float(function(x)) for x in inputs]).to(device)
- #   print(targets.mean())
- #   quit()
-     inputNum = torch.LongTensor([makeBitTensor(x,N) for x in inputs]).to(device).t()
-     positional = torch.LongTensor(list(range(0, N))).unsqueeze(1).expand(-1, batch_size).to(device)
-     num_embed = embeddings(inputNum)
-     pos_embed =  positional_embeddings(positional)
-     #print("shape num embeddings: " + str(num_embed.shape) + ", pos_embed: " + str(pos_embed.shape))
-
-     inputTensorEmbed = torch.cat([num_embed, pos_embed], dim=2)
-     hidden = qrnn(inputTensorEmbed)[0]
-     result = (output(hidden)).view(-1)
-#    print(result.size())
-     result = result.view(batch_size)
-     loss = (result - targets).pow(2).mean()
-     movAvg = 0.99 * movAvg + (1-0.99) * (float(loss))
-     #if iteration % 10 == 0 and True:
-       #print(iteration, movAvg / (1-0.99**(iteration+1)), N, sum([float(x.data.pow(2).sum()) for x in parameters() if x.grad is not None]))
-     (loss).backward()
-     optimizer.step()
-#     print(iteration, abs(log(iteration+1)/log(10) % 1))
-#     print(iteration, movAvg)
-     if movAvg < 0.003 and False:
-        lossesAfterIterations.append(movAvg)
-        lossesAfterIterations.append(movAvg)
-        lossesAfterIterations.append(movAvg)
-        lossesAfterIterations.append(movAvg)
-        lossesAfterIterations.append(movAvg)
-        lossesAfterIterations.append(movAvg)
-        lossesAfterIterations.append(movAvg)
-        break 
-     if (iteration-1) % 1000 == 0:
-        val_loss = validate(qrnn,output, function, num_samples=10000)
-
-        print(f"Iteration: {iteration}, AvgLoss: {movAvg:.3f}, Loss: {loss.detach():.3f}, Validation Loss: {val_loss:.3f}")
-        #print(f"Iteration: {iteration}, AvgLoss: {movAvg:.3f}, Loss: {loss.detach():.3f}")
-
-   return val_loss 
-
-
-import random
-myID = random.randint(1000,10000000)
-with open(f"losses_{__file__}_{myID}.csv", "w") as outFile:
-  #print(",".join(["AverageDegree", "Iterations", "Weights1", "Weights2", "PerturbedLoss", "Acc100", "Acc1000", "Acc10000", "Acc100000"]), file=outFile)
-  for _ in range(10000):
-   
-   averageDegree = N
-   coefficients = torch.randn(N,N).to(device)
-   mask = torch.zeros(N, N, dtype=torch.uint8).to(device)
+def rboolf_old(N, deg=2):
+   coefficients = torch.randn(N,N).cuda()
+   mask = torch.zeros(N, N, dtype=torch.uint8).cuda()
     
    indices = torch.randperm(N*N)[:N]  # Shuffle flattened indices and pick first N
    rows = indices // N  # Convert to 2D row indices
@@ -183,29 +83,131 @@ with open(f"losses_{__file__}_{myID}.csv", "w") as outFile:
    mask[rows, cols] = 1
    coefficients = (coefficients * mask)
    coefficients = (coefficients-coefficients.mean())/ coefficients.pow(2).sum().sqrt()
+   
    def function(x):
        binary = formatted_binary = f"{x:0{N}b}"
        c = torch.zeros(N,N)
        r = 0
        for w in range(N):
            i, j = rows[w], cols[w]
- #       for i in range(N):
- #          for j in range(N):
+    #       for i in range(N):
+    #          for j in range(N):
            c[i,j] = 1 if binary[i] == binary[j] else -1
-       r = (coefficients*c.to(device)).sum()
+       r = (coefficients*c.cuda()).sum()
        return r
-   loss = fitNetwork(function, N)
-   print(loss)
-   print(",".join([str(x) for x in (loss)]), file=outFile)
-   outFile.flush()
+   return function
 
 
-# Try different average degress, instead of just pairs. Still maintain low degree. Say deg < 5-10.
-# Vary the proportion of hidden elements that is pos. emb.
-# MLP width start big
-# keep d fixed, N fixed
-# vary l (initially 1), vary f (3*df+1) a bit
-# h = 1
-# Get rid of layer norm, get more boolean functions, matrix dot product, pos. enc. 
+def rboolf(N, width, deg):
+    coefficients = torch.randn(width).to(device)
+    coefficients = (coefficients-coefficients.mean())/coefficients.pow(2).sum().sqrt()
+    
+    combs = torch.tensor(list(itertools.combinations(torch.arange(N+1), deg))).to(device)
+    combs = combs[torch.randperm(len(combs))][:width] # Shuffled
 
-# Generate a bunch of functions, train transformer on them, report generalization error
+    def func(x):
+        binary = f"{x:0{N}b}"+"0"
+        comps = []
+        for elem in combs:
+            res = 1
+            for e in elem:
+                bit = 1 if int(binary[e]) else -1
+                res *= bit
+            comps.append(res)
+        return torch.dot(coefficients, torch.tensor(comps, dtype=torch.float32).to(device))
+    return func, (coefficients, combs)
+
+def validate(model, func, num_samples=1000):
+      model.eval()
+      inputs = torch.tensor([random.randint(0, 2**args.N-1) for _ in range(num_samples)]).to(device)
+      targets = torch.FloatTensor([float(func(x)) for x in inputs]).to(device)
+      result = model(inputs).to(device)
+      loss = (result - targets).pow(2).mean()
+      return loss.detach()
+
+def generate_dataset(num_samples, N, batch_size):
+    inputs = torch.tensor([random.randint(0, 2**N-1) for _ in range(int(num_samples))]).to(device)
+    train_loader = DataLoader(inputs, shuffle=True, batch_size=batch_size)
+    return train_loader
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='linear spectrum non boolean test.')
+    parser.add_argument('--N', type=int, default=10)
+    parser.add_argument('--n_devices', type=int, default=1)
+    parser.add_argument('--width', type=int, default=10)
+    parser.add_argument('--dim', type=int, default=20)
+    parser.add_argument('--f', type=int, default=64)
+    parser.add_argument('--l', type=int, default=1)
+    parser.add_argument('--h', type=int, default=1)
+    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--bs', type=int, default=32)
+    parser.add_argument('--num_samples', type=float, default=100000)
+    parser.add_argument('--repeat', type=int, default=100)
+
+
+    return parser.parse_args()
+
+def main(args):
+    # summary = pd.DataFrame(columns=["deg", "width", "func", "iter", "loss"])
+    losses = {}
+    func_per_deg = args.repeat
+    main_dir = f"N{args.N}_HidDim{args.dim}_L{args.l}_H{args.h}_FFDim{args.f}_lr3e5_s1000_smallbatch"
+    os.makedirs(main_dir, exist_ok=True)
+  # with open("logs_width.txt", "a") as f:
+  #   f.write("------------------------------------------\n")
+    print("generating dataset with " + str(args.num_samples)+" records. ")
+    train_loader = generate_dataset(args.num_samples, args.N, args.bs)
+    torch.save(train_loader,main_dir+"/train_dataloader.pt")
+    for i in range(func_per_deg):
+        for deg in [1,2,3,4,5]:
+            losses[deg] = []
+            #for width in range(1, args.N, 3):
+            for width in [1,4,10]:
+              print(f"Generating: func {i}, deg {deg}, width {width}")
+
+              # Create new directory to save results for the particular function
+              dir_name = os.path.join(main_dir, f"deg{deg}_width{width}_func{i}")
+              os.makedirs(dir_name, exist_ok=True)
+      
+              # Generate function and save its coefficients
+              #func = rboolf_old(args.N,  deg)
+
+              func, (coeffs, combs) = rboolf(args.N, width, deg)
+              torch.save(coeffs, f"{dir_name}/func_coeffs.pt")
+              torch.save(combs, f"{dir_name}/func_combs.pt")
+              # Generate the training dataset
+              
+              print(f"fitting function: func {i}, deg {deg}, width {width}")
+
+              # Fit the model
+              model, func_summary   = fitNetwork(func, train_loader, epochs=args.epochs, N=args.N, dir_name=dir_name,n_devices=args.n_devices)
+              func_summary["deg"]   = deg
+              func_summary["width"] = width
+              func_summary["func"]  = i
+              # func_summary = func_summary[summary.columns.tolist()]
+              # summary = pd.concat([summary, func_summary])
+              # summary.to_csv(f"{main_dir}/test.csv")
+              summary_csv = f"{main_dir}/summary.csv"
+              func_summary.to_csv(summary_csv, mode='a', header=not os.path.exists(summary_csv), index=False)
+
+        
+              # Get Test Loss
+              model.eval()
+              loss = validate(model=model, func=func, num_samples=1000)
+              losses[deg].append(loss.item())
+              # print(f"\nReported Loss: {loss.item()}\n")
+        
+              # # Write to Logs
+              # with open("logs_width.txt", "a") as f:
+              #     f.write(f"\nReported Loss: {loss.item()}\n")
+    print(losses)
+  
+    # Save to File
+    df = pd.DataFrame.from_dict(losses)
+    os.makedirs(dir_name, exist_ok=True)
+    df.to_csv(f"{main_dir}/results_layernorm_test.csv")
+
+if __name__ == "__main__":
+    args = parse_args()
+    print(args)
+    main(args)   
