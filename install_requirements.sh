d77f6bdd3559855f866f14a705191c9ce5992169
Paul Lintilhac <plintilhac@tony-3.coli.uni-saarland.de>
2025-11-06 19:20:09 +0100

    switching to using git instead of condor copy during ssh

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,3 +1,5 @@
 python3 -m ensurepip --default-pip
 python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
 python3 -m pip install pyhessian pandas
+git clone https://github.com/paullintilhac/boolean_function_sensitivity.git
+cd boolean_function_sensitivity
