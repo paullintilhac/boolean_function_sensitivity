4817f027ebb57f41c0cc3dc93434579a840d11fb
Paul Lintilhac <plintilhac@submit.coli.uni-saarland.de>
2025-11-25 00:39:30 +0100

    added job submit files

--- job_wrapper.sh
+++ job_wrapper.sh
@@ -45,11 +45,11 @@ python3 exp_refactor.py  --N 20 \
  --dim 2 \
  --h 1 \
  --f 128 \
- --bs 64 \
+ --bs 32 \
  --epochs 1000000 \
- --num_samples 8192 \
+ --num_samples 16384 \
  --repeat 1 \
- --lr "4e-4"\
+ --lr "1e-3"\
  --dropout 0.1 \
  --wd .0001 \
  --world_size 8 \
